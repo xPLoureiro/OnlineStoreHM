c2a4d090abae657dc1943d196b23212d7b17ccef
xPLoureiro <xploureiro@gmail>
2025-01-06 13:27:09 +0000

    20260106_1314

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MatMenuModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'phm';
+  title: string = 'phm';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { WishlistComponent } from './pages/wishlist/wishlist.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { AuthGuard } from './guard/auth.guard';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home Page' },
+  { path: 'home', redirectTo: '' },
+  { path: 'products', component: ProductsComponent, title: 'Produto' },
+  { path: 'products/:id', component: ProductDetailComponent, title: 'Detalhes sobre o produto' },
+  { path: 'register', component: RegisterComponent, title: 'Registo' },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard], title: 'Profile' },
+  { path: 'wishlist', component: WishlistComponent, canActivate: [AuthGuard], title: 'Whishlist' },
+  { path: 'cart', component: CartComponent, canActivate: [AuthGuard], title: 'Carrinho' },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard], title: 'Admin' },
+  { path: '**', component: NotfoundComponent, title: 'Page not found'}
+];
